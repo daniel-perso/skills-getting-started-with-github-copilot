4d07fc16c0f3ce4cfdc1d3859c2a0d0a6d5b880f
daniel-perso <daniel.desrosiers@gmail.com>
2026-03-13 19:23:06 +0000

    Ajout de la section des participants avec style pour afficher les participants inscrits aux activités

--- src/static/app.js
+++ src/static/app.js
@@ -20,11 +20,26 @@ document.addEventListener("DOMContentLoaded", () => {
 
         const spotsLeft = details.max_participants - details.participants.length;
 
+        // Création de la liste des participants
+        let participantsHTML = "<ul class='participants-list'>";
+        if (details.participants.length === 0) {
+          participantsHTML += "<li class='no-participant'>Aucun participant pour l'instant</li>";
+        } else {
+          details.participants.forEach(email => {
+            participantsHTML += `<li>${email}</li>`;
+          });
+        }
+        participantsHTML += "</ul>";
+
         activityCard.innerHTML = `
           <h4>${name}</h4>
           <p>${details.description}</p>
           <p><strong>Schedule:</strong> ${details.schedule}</p>
           <p><strong>Availability:</strong> ${spotsLeft} spots left</p>
+          <div class="participants-section">
+            <strong>Participants&nbsp;:</strong>
+            ${participantsHTML}
+          </div>
         `;
 
         activitiesList.appendChild(activityCard);
